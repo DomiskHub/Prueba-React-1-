2f5ac70dcc03cd361b73efc424c8b8694159e8ea
Dominque Piffardi <domiskp@gmail.com>
2023-10-23 21:56:10 -0300

    catch added

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -11,10 +11,10 @@ const MiApi = () => {
       const res = await fetch("https://hp-api.onrender.com/api/characters");
       const json = await res.json();
       const filterImg = json.filter((ch) => ch.image !== "");
-      setCharacters([...filterImg]);
-      setFilteredCharacters([...filterImg]);
+      setCharacters([...filterImg]); //estado que tiene todos los characters con spread operator para copiar el array
+      setFilteredCharacters([...filterImg]); // estado que tiene filtro
     } catch (e) {
-      console.log(e);
+      alert("Error en la API");
     }
   };
 
